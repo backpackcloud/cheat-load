df79bc55c760027dc60a5da5b47fe1bbb9ca7985
Marcelo Guimarães <ataxexe@backpackcloud.com>
2021-04-06 23:23:40 +0200

    remove unused method

--- src/main/java/com/backpackcloud/cheatload/Broadcaster.java
+++ src/main/java/com/backpackcloud/cheatload/Broadcaster.java
@@ -4,8 +4,4 @@ public interface Broadcaster {
 
   void broadcast(String eventName, Object object);
 
-  default void broadcast(JobEvent jobEvent) {
-    broadcast(jobEvent.event(), jobEvent);
-  }
-
 }
